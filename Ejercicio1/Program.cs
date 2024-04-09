12f63992e6280302c7701eeb3f8c1dfa7976181c
AlbarracinMaximo <150526045+AlbarracinMaximo@users.noreply.github.com>
2024-04-08 22:29:27 -0300

    ejercicio

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroIngresado;
+            bool estado;
+            float valorIngresado;
+
+            numeroIngresado = Console.ReadLine();
+            estado = int.TryParse(valorIngresado, out numeroIngresado);
+
+            if (numeroIngresado > 0 && estado)
+            {
+                Math.Pow(numeroIngresado, 2);
+                Math.Pow(numeroIngresado, 3);
+                Console.WriteLine()
+            }
+
+
+        }
+    }
+}
